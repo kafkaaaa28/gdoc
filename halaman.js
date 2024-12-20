90ada027bcb53684e7420c7cd569dba287be1389
kafkaaaa28 <kafkafj1@gmail.com>
2024-12-20 22:47:10 +0700

    update modal dan localstorage

--- halaman.js
+++ halaman.js
@@ -5,13 +5,22 @@ let border = document.getElementById('border');
 let loader = document.createElement('div');
 let load = document.createElement('div');
 let bg = document.getElementById('bg');
+let comming = document.getElementById('comming');
+
 formregister.style.display = 'none';
 loader.appendChild(load);
 bg.appendChild(loader);
 
+function closex() {
+  border.classList.add('tracking-out-contract-bck-top');
+  setTimeout(() => {
+    border.style.display = 'none';
+  }, 500);
+}
 let daftar = document.getElementById('daftar');
 daftar.addEventListener('click', function () {
   border.classList.add('text-blur-out');
+
   setTimeout(() => {
     formlogin.style.display = 'none';
     border.classList.add('text-focus-in');
@@ -74,7 +83,10 @@ let trans = document.getElementById('transparent');
 mymodal.style.display = 'none';
 
 function tutup() {
-  mymodal.style.display = 'none';
+  mymodal.classList.add('tracking-out-contract-bck-top');
+  setTimeout(() => {
+    mymodal.style.display = 'none';
+  }, 500);
   trans.style.backgroundColor = 'white';
   trans.classList.remove('no-interaction');
 }
@@ -82,26 +94,45 @@ function tutup() {
 function login() {
   let passlogin = document.getElementById('pass-login').value;
   let userlogin = document.getElementById('user-login').value;
+  let massagecontent = document.querySelector('.massage-content');
 
-  if (userlogin == 'admin' && passlogin == 'admin123') {
+  if (userlogin.trim() === 'admin' && passlogin.trim() === 'admin123') {
     localStorage.setItem('sebagai', 'admin');
     localStorage.setItem('username', userlogin);
     localStorage.setItem('password', passlogin);
+    border.style.display = 'none';
+    comming.textContent = `Comming soon`;
+  } else if (localStorage.getItem('username') === userlogin.trim() && localStorage.getItem('password') === passlogin.trim()) {
+    console.log(`user = ${userlogin} pw = ${passlogin}`);
+    border.style.display = 'none';
+    comming.textContent = `Comming soon`;
   } else {
-    localStorage.setItem('modalStatus', 'open');
     trans.style.backgroundColor = 'rgba(0, 0, 0, 0.5)';
+    mymodal.classList.add('tracking-in-expand-fwd-top');
+    mymodal.classList.remove('tracking-out-contract-bck-top');
+    massagecontent.textContent = 'The username or password is incorrect, or you have not registered'.toUpperCase();
     mymodal.style.display = 'block';
     trans.classList.add('no-interaction');
   }
 }
-window.onload = function () {
-  if (localStorage.getItem('modalStatus') === 'open') {
+
+function register() {
+  let userregis = document.getElementById('user-register').value;
+  let passregis = document.getElementById('pass-register').value;
+  let massagecontent = document.querySelector('.massage-content');
+  let alert = document.getElementById('alert');
+  if (userregis && passregis) {
+    localStorage.setItem('sebagai', 'username');
+    localStorage.setItem('username', userregis);
+    localStorage.setItem('password', passregis);
+    alert.classList.add('text-focus-in');
+    alert.textContent = `you have successfully registered`;
+  } else {
     trans.style.backgroundColor = 'rgba(0, 0, 0, 0.5)';
+    mymodal.classList.add('tracking-in-expand-fwd-top');
+    mymodal.classList.remove('tracking-out-contract-bck-top');
+    massagecontent.textContent = 'please fill in all fields'.toUpperCase();
     mymodal.style.display = 'block';
     trans.classList.add('no-interaction');
-  } else {
-    mymodal.style.display = 'none';
   }
-
-  localStorage.removeItem('modalStatus');
-};
+}
